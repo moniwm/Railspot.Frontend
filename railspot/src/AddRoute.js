bb32d4d38b96b1c9a4957df4f2224b52495ef9e1
Juan Igancio Navarro <juannava0208@gmail.com>
2020-08-12 22:32:28 -0600

    Add route created

--- railspot/src/AddRoute.js
+++ railspot/src/AddRoute.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from "./Nav";
+import "./Admin.css";
+import DeleteImage from "./images/deleteRoute.png"
+
 
 export default function AddRoute(){
+
+    const [input, setInput] = useState();
+
+    const updateInput = e =>{
+        setInput (e.target.value);
+    }
+
+    const postStation = e =>{
+        e.preventDefault();
+        // var init={
+        //     method: 'POST',
+        //     mode: 'no-cons',
+        // };
+        fetch(`http://localhost:8080/RailSpot.BackEnd/api/admin/new-station?name=${input}`, {method: 'POST', mode:'no-cors'})
+        .then(function(response){
+            console.log(response);
+        });
+    }
+
     return(
-        
+
         <div>
             <Nav />
+            <div className="Page">
+                <section className="Box">
+                    <img src={DeleteImage}></img>
+                    
+                    <div>
+                        <h3 className="RouteText" >Station Name</h3>
+                        <input onChange={updateInput} className="RouteInput" type="text"></input>
+                    </div>
+                    
+                    <form onSubmit={postStation}>
 
-            <input></input>
+                        <button type="submit" className="RouteButton">ADD STATION</button>
 
-          
+                    </form>
+
+                </section>
+
+            </div>
             
         </div>
-
        
     )
 }
