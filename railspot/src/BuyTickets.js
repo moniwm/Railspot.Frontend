79639bc758596ce67de32d4a461ff408bc069035
Monica Waterhouse <mwaterhouse@estudiantec.cr>
2020-08-15 12:46:55 -0600

    Set the parameters to buy a ticket and request to post a ticket

--- railspot/src/BuyTickets.js
+++ railspot/src/BuyTickets.js
@@ -13,6 +13,7 @@ import Select from "@material-ui/core/Select";
 import Typography from "@material-ui/core/Typography";
 import DateFnsUtils from "@date-io/date-fns";
 import axios from "axios";
+import Input from '@material-ui/core/Input';
 
 import {
   MuiPickersUtilsProvider,
@@ -68,11 +69,15 @@ export default function BuyTickets() {
     new Date("2020-08-15T21:11:54")
   );
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
+  const [stations, setStation] = useState([]);
 
-  const[stations, setStation] = useState([]);
+  const [startLocation, setStartLocation] = useState("");
+  const [destiny, setDestiny] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [time, setTime] = useState("");
+  const [discount, setDiscount] = useState("");
+  const [price, setPrice] = useState("");
+  const [selectedStation, setSelectedStation] = useState([""]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -90,20 +95,49 @@ export default function BuyTickets() {
     fetchData();
   }, []);
 
+  const handleStartLocation = (event) => {
+    setStartLocation(event.target.value);
+  };
+
+  const handleDestiny = (event) => {
+    setDestiny(event.target.value);
+  };
+
+  const handleQuantity = (event) => {
+    setQuantity(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleTime = (event) => {
+    setTime(event.target.value);
+  }
+
+  function notRepeat() {
+    if (startLocation === destiny) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const postTicket = e =>{
+    e.preventDefault();
+    fetch(`http://localhost:8080/RailSpot.BackEnd/api/tickets/buy-ticket?user=1&origin=${startLocation}&destiny=${destiny}&date=${selectedDate}&hour=${time}&amount=${quantity}&From=1&Authorization=password`, {method: 'POST', mode:'no-cors'})
+    .then(function(response){
+        console.log(response);
+    });
+}
+
+
   return (
     <div>
       <Nav />
       <Grid container component="main" className={classes.root}>
         <CssBaseline />
-        <Grid item xs={false} sm={4} md={7} className={classes.routes}>
-        <ul>
-      {stations.map(item => (
-        <li>
-          {item.name}
-        </li>
-      ))}
-    </ul>
-        </Grid>
+        <Grid item xs={false} sm={4} md={7} className={classes.routes}></Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <div className={classes.paper}>
             <Typography component="h1" variant="h5">
@@ -119,13 +153,13 @@ export default function BuyTickets() {
                   <Select
                     labelId="demo-controlled-open-select-label"
                     id="demo-controlled-open-select"
+                    value={startLocation}
+                    onChange={handleStartLocation}
                   >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+      
+                    {stations.map((item) => (
+                      <MenuItem value={item.name}>{item.name}</MenuItem>
+                    ))}
                   </Select>
                 </FormControl>
               </Grid>
@@ -137,35 +171,35 @@ export default function BuyTickets() {
                   <Select
                     labelId="demo-controlled-open-select-label"
                     id="demo-controlled-open-select"
+                    value={destiny}
+                    onChange={handleDestiny}
                   >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+        
+                    {stations.map((item) => (
+                      <MenuItem value={item.name}>{item.name}</MenuItem>
+                    ))}
                   </Select>
                 </FormControl>
               </Grid>
 
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12}>
                 <FormControl className={classes.formControl}>
-                  <InputLabel id="demo-controlled-open-select-label">
-                    Quantity
-                  </InputLabel>
-                  <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                  </Select>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date you want to travel"
+                format="MM-dd-yyyy"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date",
+                }}
+              />
+            </MuiPickersUtilsProvider>
                 </FormControl>
-              </Grid>
+              
+            </Grid>
 
               <Grid item xs={12} sm={6}>
                 <FormControl className={classes.formControl}>
@@ -175,35 +209,55 @@ export default function BuyTickets() {
                   <Select
                     labelId="demo-controlled-open-select-label"
                     id="demo-controlled-open-select"
+                    value = {time}
+                    onChange={handleTime}
                   >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                    <MenuItem value={'7:00'}>7:00</MenuItem>
+                    <MenuItem value={'8:00'}>8:00</MenuItem>
+                    <MenuItem value={'9:00'}>9:00</MenuItem>
+                    <MenuItem value={'10:00'}>10:00</MenuItem>
+                    <MenuItem value={'12:00'}>12:00</MenuItem>
+                    <MenuItem value={'14:00'}>14:00</MenuItem>
+                    <MenuItem value={'16:00'}>16:00</MenuItem>
+                    <MenuItem value={'17:00'}>17:00</MenuItem>
+                    <MenuItem value={'18:00'}>18:00</MenuItem>
+                    <MenuItem value={'19:00'}>19:00</MenuItem>
+
+                    
                   </Select>
                 </FormControl>
               </Grid>
+
+              <Grid item xs={12} sm={6}>
+              <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Quantity
+                  </InputLabel>
+                  <Input
+            className={classes.input}
+            value={quantity}
+            margin="dense"
+            onChange={handleQuantity}
+            inputProps={{
+              step: 1,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+                </FormControl>
+
+              
+              </Grid>
+
             </Grid>
 
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <KeyboardDatePicker
-                margin="normal"
-                id="date-picker-dialog"
-                label="Date you want to travel"
-                format="MM/dd/yyyy"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-              />
-            </MuiPickersUtilsProvider>
+            
           </div>
 
           <Grid className={classes.paper}>
-            <Button variant="outlined" color="primary">
+            <Button variant="outlined" color="primary" onClick={postTicket}>
               Buy tickets
             </Button>
           </Grid>
